9bc8c4e81c2d8170d1be160b7af5cc94c7189f52
dmitryhusev <dimtri.gusev@gmail.com>
2024-05-04 00:44:56 +0000

    test

--- tests/test_home_page.py
+++ tests/test_home_page.py
@@ -1,6 +1,10 @@
 
 
+from tests.logger import LOG
+
+
 def test_main(browser):
+    LOG.info('Open page')
     browser.page.goto('https://alib.com.ua')
     assert 'Alib1' in browser.page.title()
 
@@ -10,5 +14,6 @@ def test_search(browser):
     browser.page.get_by_role("textbox").fill("knut")
     browser.page.get_by_role("textbox").press("Enter")
     text = "Если ничего не найдено, укажите только автора или одно слово из названия. Примеры поиска"
+    LOG.info('Check text')
     browser.set_expect_timeout(5000).expect(browser.locator("//p[3]")).to_contain_text(text)
 
